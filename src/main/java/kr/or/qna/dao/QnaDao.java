fec44ed996af645d94541d69a173713b82aeb2b6
User <User@DESKTOP-BJNQG2H>
2021-11-30 21:58:32 +0900

    qnaDao 어노테이션 추가 from 민주

--- src/main/java/kr/or/qna/dao/QnaDao.java
+++ src/main/java/kr/or/qna/dao/QnaDao.java
@@ -2,7 +2,9 @@ package kr.or.qna.dao;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class QnaDao {
 
 	@Autowired
