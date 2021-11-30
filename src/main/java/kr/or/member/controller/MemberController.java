f52edac667f0b8432858d60ac936b8342a2f0f20
1amthomas <1amthomas@DESKTOP-BC85IAU>
2021-11-30 15:33:51 +0900

    - 밀린 파일 다시 추가함 - 진호

--- src/main/java/kr/or/member/controller/MemberController.java
+++ src/main/java/kr/or/member/controller/MemberController.java
@@ -1,13 +1,39 @@
 package kr.or.member.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import kr.or.member.model.service.MemberService;
+import kr.or.member.model.vo.Member;
 
 @Controller
 public class MemberController {
+	@Autowired
+	private MemberService service;
 	
 	@RequestMapping(value="/login.do")
 	public String login() {
 		return "member/login";
+	}
+	@RequestMapping(value="/loginFrm.do")
+	public String loginFrm(Member member,HttpSession session,Model model) {
+		Member m = service.selectOneMember(member);
+		if(m != null) {
+			session.setAttribute("m", m);
+			model.addAttribute("msg","로그인성공");
+		}else {
+			model.addAttribute("msg","아이디 또는 비밀번호를 확인하세요");	
+		}
+		model.addAttribute("loc","/");
+		return "common/msg";
+	}
+	@RequestMapping(value="/logout.do")
+	public String logout(HttpSession session) {
+		session.invalidate();
+		return "common/main";
 	}
 }
