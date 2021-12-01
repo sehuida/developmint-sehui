dd03c4ce028c1f24bede2fbda8584fa2932cd978
MINU JEONG <min9288@gmail.com>
2021-12-01 14:06:08 +0900

    풀하려고 커밋

--- src/main/java/kr/or/projectTeam/controller/ProjectTeamController.java
+++ src/main/java/kr/or/projectTeam/controller/ProjectTeamController.java
@@ -1,13 +1,19 @@
 package kr.or.projectTeam.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import kr.or.projectTeam.model.service.ProjectTeamService;
+
 @Controller
 public class ProjectTeamController {
+	@Autowired
+	private ProjectTeamService service;
 	
 	@RequestMapping(value="/recruitTeamMember_mainPage.do")
 	public String recruitTeamMember() {
+		
 		return "recruitCrue/recruitTeamMember_mainPage";
 	}
 }
