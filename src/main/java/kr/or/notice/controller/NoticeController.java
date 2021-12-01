d9c7cd83a1bcce19610dd7b68420610dce676b9e
User <User@DESKTOP-BJNQG2H>
2021-12-01 18:25:52 +0900

    noticelist.jsp, css, controller, default.css 수정 from 민주

--- src/main/java/kr/or/notice/controller/NoticeController.java
+++ src/main/java/kr/or/notice/controller/NoticeController.java
@@ -1,10 +1,14 @@
 package kr.or.notice.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.or.notice.service.NoticeService;
+import kr.or.notice.vo.Notice;
 
 @Controller
 public class NoticeController {
@@ -13,7 +17,11 @@ public class NoticeController {
 	private NoticeService service;
 	
 	@RequestMapping(value="/noticeList.do")
-	public String noticeList() {
+	public String noticeList(Model model) {
+		/*
+		 * ArrayList<Notice> list = service.selectNoticeList();
+		 * model.addAttribute("list",list);
+		 */
 		return "notice/noticeList";
 	}
 	
