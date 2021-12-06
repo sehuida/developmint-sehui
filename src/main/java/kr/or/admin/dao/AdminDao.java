fbd51141a5e43c2454353898048718707e0dd6b0
hyoen1106 <83644608+hyoen1106@users.noreply.github.com>
2021-12-06 17:50:25 +0900

    관리자페이지 대시보드 오류 해결 - 수현

--- src/main/java/kr/or/admin/dao/AdminDao.java
+++ src/main/java/kr/or/admin/dao/AdminDao.java
@@ -7,7 +7,6 @@ import java.util.List;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
