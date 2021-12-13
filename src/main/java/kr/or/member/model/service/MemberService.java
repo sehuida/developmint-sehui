4bdc8d196b8c40284284394378f9cde10bb1cf7b
1amthomas <1amthomas@DESKTOP-BC85IAU>
2021-12-13 15:01:00 +0900

    공모전신청내역 게시판 구현완료 - 진호

--- src/main/java/kr/or/member/model/service/MemberService.java
+++ src/main/java/kr/or/member/model/service/MemberService.java
@@ -9,12 +9,17 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import kr.or.contest.vo.Contest;
+import kr.or.contest.vo.ContestList;
+import kr.or.contest.vo.ContestMember;
 import kr.or.gosu.vo.GosuNotice;
 import kr.or.member.controller.RandomPassword;
 import kr.or.member.model.dao.MemberDao;
 import kr.or.member.model.vo.CertiVO;
+import kr.or.member.model.vo.ContestPage;
 import kr.or.member.model.vo.GosuNoticePage;
 import kr.or.member.model.vo.Member;
+import kr.or.member.model.vo.MyContest;
 
 @Service
 public class MemberService {
@@ -167,6 +172,59 @@ public class MemberService {
 				
 				GosuNoticePage gnp = new GosuNoticePage(list,pageNavi,start);
 				return gnp;
+	}
+
+	public ContestPage contestList(Member m, int reqPage) {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		int numPerPage = 5;
+		int end = reqPage*numPerPage;
+		int start = end - numPerPage + 1;
+		
+		map.put("memberId", m.getMemberId());
+		map.put("start",start);
+		map.put("end",end);
+		ArrayList<MyContest> list = dao.contestMemberList(map);
+		
+		int totalCount = dao.contestCount(m.getMemberId());
+		int totalPage = 0;
+		if(totalCount%numPerPage == 0) {
+			totalPage = totalCount/numPerPage;
+		}else {
+			totalPage = totalCount/numPerPage+1;
+		}
+		int pageNaviSize = 5;
+		int pageNo = ((reqPage-1)/pageNaviSize)*pageNaviSize+1;
+		
+		String pageNavi = "<ul class='pagination pagination-lg'>";
+				
+				if(pageNo != 1) {
+					pageNavi += "<li class=\"page-item\">";
+					pageNavi += "<a class=\"page-link\" href='/mycontestPage.do?reqPage="+(pageNo-1)+"&memberId="+(m.getMemberId())+"'>&laquo;</a></li>";
+				}
+				
+				for(int i=0;i<pageNaviSize;i++){
+					if(pageNo == reqPage) {
+						pageNavi += "<li class=\"page-item active\">";
+						pageNavi += "<a class='page-link' href='/mycontestPage.do?reqPage="+pageNo+"&memberId="+(m.getMemberId())+"'>"+pageNo+"</a></li>";
+					} else {
+						pageNavi += "<li class='page-item'>";
+						pageNavi += "<a class='page-link' href='/mycontestPage.do?reqPage="+pageNo+"&memberId="+(m.getMemberId())+"'>";
+						pageNavi += pageNo+"</a></li>";
+					}
+					pageNo++;
+					if(pageNo>totalPage) {
+						break;
+					}
+				}
+				if(pageNo <= totalPage) {
+					pageNavi += "<li class='page-item'>";
+					pageNavi += "<a class='page-link' href='/mycontestPage.do?reqPage="+pageNo+"&memberId="+(m.getMemberId())+"'>";
+					pageNavi += "&raquo;</a></li>";
+				}
+				pageNavi += "</ul>";
+				
+				ContestPage ctp = new ContestPage(list, pageNavi, start);
+				return ctp;
 	}
 	
 }
