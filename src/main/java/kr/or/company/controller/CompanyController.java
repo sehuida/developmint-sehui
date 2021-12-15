a0b4f80ada1ce2e019fdb6d41b33067008b71d13
user1 <user1@DESKTOP-8FG99D4>
2021-12-15 18:24:10 +0900

    회사정보 가져오는거 쿼리문은 완성인데 announceTitle이 null임 - 슬기

--- src/main/java/kr/or/company/controller/CompanyController.java
+++ src/main/java/kr/or/company/controller/CompanyController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.or.company.service.CompanyService;
 import kr.or.company.vo.Company;
+import kr.or.company.vo.CompanyInfo;
 
 @Controller
 public class CompanyController {
@@ -17,7 +18,7 @@ public class CompanyController {
 	@RequestMapping(value="/companyInfo.do")
 	public String companyInfo(String companyName, Model model) {
 		//회사명으로 company정보 가져오기
-		Company company = service.selectCompany(companyName);
+		CompanyInfo company = service.selectCompany(companyName);
 		model.addAttribute("com", company);
 		System.out.println("company : "+ company);
 		return "jobSearch/companyInfo";
---
+++ src/main/java/kr/or/company/vo/CompanyInfo.java
@@ -0,0 +1,22 @@
+package kr.or.company.vo;
+
+import lombok.Data;
+
+@Data
+public class CompanyInfo {
+	private String companyTitle;
+	private int career;
+	private int school;
+	private String workPlace;
+	private int workForm;
+	private int companyNo;
+	private String companyName;
+	private String openDate;
+	private int employee;
+	private String category;
+	private String ceo;
+	private String address;
+	private String content;
+	private int businessNo;
+
+}
