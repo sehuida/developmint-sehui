0dca928ac84c7e8bec6df577b2b27a9ad21070db
User <User@DESKTOP-BJNQG2H>
2021-12-16 18:20:21 +0900

    qna 회원/비회원 insert완료 from 민주(파일첨부)

--- src/main/java/kr/or/qna/controller/QnaController.java
+++ src/main/java/kr/or/qna/controller/QnaController.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -20,7 +21,9 @@ import org.springframework.web.multipart.MultipartFile;
 
 import kr.or.member.model.vo.Member;
 import kr.or.notice.vo.Notice;
+import kr.or.projectTeam.model.vo.ProjectTeamFileVO;
 import kr.or.qna.service.QnaService;
+import kr.or.qna.vo.Qna;
 import kr.or.qna.vo.nonQna;
 
 @Controller
@@ -120,6 +123,67 @@ public class QnaController {
 			model.addAttribute("msg","1:1 Q&A 신청 실패");
 		}
 		model.addAttribute("loc","/n_counsel.do");
+		return "common/msg";
+	}
+	
+	@RequestMapping(value="/counsel_save2.do")
+	public String insertCounsel2(@SessionAttribute Member m, Qna q, HttpServletRequest request, MultipartFile[] Filedata, Model model) {
+		ArrayList<ProjectTeamFileVO> list = new ArrayList<ProjectTeamFileVO>();
+		//MultipartFile[]배열은 파일을 첨부하지 않더라도 무조건 길이가 1인 배열
+		//배열의 첫번째 파일이 비어있는지 체크하는 방식으로 파일첨부 여부 확인
+		if(!Filedata[0].isEmpty()) {	
+			//파일을 업로드할 경로 설정
+			String savePath = request.getSession().getServletContext().getRealPath("/resources/upload/qna/");
+			//반복문을 이요해서 파일처리(파일업로드)
+			for(MultipartFile file : Filedata) {
+				//파일명중복처리
+				String filename = file.getOriginalFilename();
+				String onlyFilename = filename.substring(0,filename.indexOf("."));//test
+				String extention = filename.substring(filename.indexOf("."));//.txt
+				//실제 업로드할 파일명을 저장할 변수
+				String filepath = null;
+				int count = 0;
+				while(true) {
+					if(count ==0) {
+						filepath = onlyFilename+extention;//test.txt
+					}else {
+						filepath = onlyFilename+"_"+count+extention;	//test_3.txt
+					}
+					File checkFile = new File(savePath+filepath);		//java.io.File
+					if(!checkFile.exists()) {//존재하지않을경우 ==>true가 되게
+						break;
+					}
+					count++;
+				}//---------------------------------------------------------------------------------
+				//파일명 중복처리가 끝나면 파일업로드
+				try {
+					FileOutputStream fos = new FileOutputStream(new File(savePath+filepath));
+					//업로드 속도증가를 위한 보조스트림
+					BufferedOutputStream bos = new BufferedOutputStream(fos);
+					//파일 업로드
+					byte[] bytes = file.getBytes();
+					bos.write(bytes);
+					bos.close();
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				ProjectTeamFileVO ptfv = new ProjectTeamFileVO();
+				ptfv.setFileName(filename);
+				ptfv.setFilePath(filepath);
+				list.add(ptfv);
+			}
+		}
+		int result = service.insertCounselQna(m,q, list);
+		if(result == -1||result != list.size()) {
+			model.addAttribute("msg","등록실패");
+		}else {
+			model.addAttribute("msg","등록성공");
+		}
+		model.addAttribute("loc","/");
 		return "common/msg";
 	}
 	
--- src/main/java/kr/or/qna/service/QnaService.java
+++ src/main/java/kr/or/qna/service/QnaService.java
@@ -1,9 +1,15 @@
 package kr.or.qna.service;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import kr.or.member.model.vo.Member;
+import kr.or.projectTeam.model.vo.ProjectTeamFileVO;
 import kr.or.qna.dao.QnaDao;
+import kr.or.qna.vo.Qna;
 import kr.or.qna.vo.nonQna;
 
 @Service
@@ -14,6 +20,27 @@ public class QnaService {
 
 	public int insertCounselNonQna(nonQna nq) {
 		int result = dao.insertCountNonQna(nq);
+		return result;
+	}
+
+	@Transactional
+	public int insertCounselQna(Member m, Qna q, ArrayList<ProjectTeamFileVO> list) {
+		System.out.println("insert 전 :"+q.getQnaNo());
+		int result1 = dao.insertCounselQna(q);
+		System.out.println("insert 후 :"+q.getQnaNo());
+		System.out.println("멤버 넘버"+m.getMemberNo());
+		int result = 0;
+		if(result1>0) {
+			for(ProjectTeamFileVO ptfv : list) {
+				ptfv.setMemberNo(m.getMemberNo());
+				ptfv.setBoardNo(q.getQnaNo());
+				System.out.println("파일"+ptfv.getFileName());
+				System.out.println("파일"+ptfv.getFilePath());
+				result += dao.insertFile(ptfv);
+			}
+		}else {
+			return -1;
+		}
 		return result;
 	}
 
