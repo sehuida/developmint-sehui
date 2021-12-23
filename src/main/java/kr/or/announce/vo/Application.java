6ffbeef0510650bd37de8841aafe34687b2669df
user1 <user1@DESKTOP-8FG99D4>
2021-12-23 16:01:23 +0900

    로그인 안했을 때 공고페이지 에러 수정, 지원하기insert 거의 완료 - 슬기

---
+++ src/main/java/kr/or/announce/vo/Application.java
@@ -0,0 +1,12 @@
+package kr.or.announce.vo;
+
+import lombok.Data;
+
+@Data
+public class Application {
+	private int appNo;
+	private int announceNo;
+	private int resumeNo;
+	private int memberNo;
+	private int status;
+}
